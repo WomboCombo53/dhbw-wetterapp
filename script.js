be32b65fc956df89da20ea6ea39a66359559600d
CL500 <CL500@JAGUAR-F-TYPE>
2024-07-11 18:24:24 +0200

    add string manip

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 class WeatherData
 {
     temp;
+    rainPossibility;
+    forecast;
+    timeOfReport;
 
-    
+    WeatherData(temp, rain, forecast, timeOfReport)
+    {
+        this.temp = temp;
+        this.rainPossibility = rain;
+        this.forecast = forecast;
+        this.timeOfReport = timeOfReport;
+    }
+    WeatherData(temp, rain, forecast)
+    {
+        WeatherData(temp, rain, forecast, new Date(Date.now()))
+    }
+    WeatherData(){}
+
+    getFahrenheit()
+    {
+        return temp * 0.56;
+    }
 }
 function ConvertDWDResponseToObject(json)
 {
     try
     {
         const forecast = json["forecast1"]
-        const temp = forecast["temperature"]["18"]
-    
+        const temp = forecast["temperature"]["18"] / 10 // temp value is multiplied by 10
+        const rainPossibility = forecast["rain"]["18"]
+
         let data = new WeatherData()
         data.temp = temp;
+        data.forecast = forecast;
+        data.rainPossibility = rainPossibility;
 
         return data;
     }
@@ -22,7 +44,11 @@ function ConvertDWDResponseToObject(json)
     }
 }
 
-const userStorage = "user"
+let openBtn;
+let closeBtnschliessen;
+let modal;
+
+const userStorageName = "user"
 const cityStorageName = "city"
 const weatherStorage = "weather"
 const favoriteStorage = "favs"
@@ -31,18 +57,27 @@ const userActivityStorage = "uAct"
 
 const defaultWeather = {"temp": 35, "desc": "Sonnig", "rain": 55};
 
+// Daten
 let users = [
-    {"username": "user", "password": "user", "isAdmin": false, "useCelsius": true},
-    {"username": "admin", "password": "admin", "isAdmin": true, "useCelsius": false}
+    {"username": "user", "password": "user", "isAdmin": false, "useFahrenheit": true},
+    {"username": "admin", "password": "admin", "isAdmin": true, "useFahrenheit": false}
 ]
 
 let cities = [
-    {"cityname": "Bonn", "apiKey": "10517", "weather": {"temp": 17, "desc": "Sonnig", "rain": 12}},
-    {"cityname": "Stuttgart", "apiKey": "Q358", "weather": {"temp": 31, "desc": "Bewölkt", "rain": 78}}
+    {"cityname": "Bonn", "apiKey": "10517", "weather": new WeatherData(17, "Sonnig", 12)},
+    {"cityname": "Stuttgart", "apiKey": "Q358", "weather": new WeatherData(31, "Bewölkt", 78)}
 ]
+function getWeatherForCity(cityname)
+{
+    for (let i = 0; i < cities.length; i++)
+    {
+        if (cities[i]["cityname"] === cityname)
+            return cities[i]["weather"];
+    }
+}
 
 let weatherReports = [
-    {"temp": 22, "generalDescription": "Bewölkt", "rainPossibility": 87, "timeOfReport": new Date(12, 12, 2023)}
+    {"user": "user", "report": new WeatherData(12, 33, "Sonnig")}
 ]
 
 let favouriteCities = [
@@ -54,17 +89,20 @@ let cityActivity = [
 ]
 
 let userActivity = [
-    {"user": "user", }
+    {"user": "user", "report": weatherReports[0]}
 ]
+
+// Angemeldeter User
+let currentUserIndex = 0;
 
 function LoadStorageData()
 {
-    let storedUsers = localStorage.getItem(userStorage)
+    let storedUsers = localStorage.getItem(userStorageName)
     let storedCities = localStorage.getItem(cityStorageName)
     let storedWeather = localStorage.getItem(weatherStorage)
     let storedFavs = localStorage.getItem(favoriteStorage)
     let storedCAct = localStorage.getItem(cityActivityStorage)
-    let storedUAct = localStorage.getITem(userActivityStorage)
+    let storedUAct = localStorage.getItem(userActivityStorage)
 
     if (storedUsers != null)
         users = JSON.parse(storedUsers)
@@ -87,7 +125,7 @@ function LoadStorageData()
 
 function SaveStorageData()
 {
-    localStorage.setItem(userStorage, JSON.stringify(users))
+    localStorage.setItem(userStorageName, JSON.stringify(users))
     localStorage.setItem(cityStorageName, JSON.stringify(cities))
     localStorage.setItem(weatherStorage, JSON.stringify(weatherReports))
     localStorage.setItem(favoriteStorage, JSON.stringify(favouriteCities))
@@ -95,17 +133,37 @@ function SaveStorageData()
     localStorage.setItem(userActivityStorage, JSON.stringify(userActivity))
 }
 
+function ShowAdminInterface(visible)
+{
+    const adminObjs = document.getElementsByClassName("admin");
+
+    for (let i = 0; i < adminObjs.length; i++)
+    {
+        let item = adminObjs.item(i);
+        item.style.visibility = visible ? "visible" : "hidden";
+    }
+}
+
 function SetInnerHTML(DOMobj, text)
 {
     DOMobj.innerHTML = text;
 }
 
+function EventFahrenheitValueChanged()
+{
+    const value = document.getElementById("fahrenheit").checked;
+    users[currentUserIndex].useFahrenheit = value;
+    SaveStorageData();
+}
+
 // von: https://stackoverflow.com/questions/831030/how-to-get-get-request-parameters-in-javascript
-function getParam(name){
+function getParam(name)
+{
     if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
        return decodeURIComponent(name[1]);
 }
 
+// API-Call
 function GetDWDWeatherData(station)
 {
     // Aufgrund von Cross-Origin-Request Beschränkungen funktioniert dieser API-Call nicht!
@@ -117,26 +175,7 @@ function GetDWDWeatherData(station)
     return request.responseText;
 }
 
-function ScriptOnDocLoad()
-{
-    SetPopup()
-    
-    const citynameObj = document.getElementById("Stadtname");
-    const tempObj = document.getElementById("Stadttemp");
-    const cityname = getParam(cityStorageName);
-
-    let weather = ConvertDWDResponseToObject(GetDWDWeatherData(cities[0]["apiKey"]))
-
-    console.log("PARAMS: " + location.search);
-    SetInnerHTML(citynameObj, cityname);
-    SetInnerHTML(tempObj, weather.temp);
-}
-
-let openBtn;
-let closeBtnschliessen;
-let modal;
-
-function SetPopup()
+function SetPopupListeners()
 {
     openBtn = document.getElementById("popupoeffnen")
     closeBtnschliessen = document.getElementById("popupschliessen")
@@ -153,4 +192,82 @@ function SetPopup()
         });
 }
 
+function SetCityName(cityname)
+{
+    const citynameObj = document.getElementById("Stadtname");
+    SetInnerHTML(citynameObj, cityname);
+}
+function SetTemperature(temp)
+{
+    const tempObj = document.getElementById("Stadttemp");
+    SetInnerHTML(tempObj, temp);
+}
+function SetRain(rain)
+{
+    const rainObj = document.getElementById("Regenwahrsch");
+    SetInnerHTML(rainObj, rain);
+}
+function SetFahrenheitCheckbox(fahrenheit)
+{
+    const fObj = document.getElementById("fahrenheit");
+    fObj.checked = fahrenheit;
+}
+
+function SetUser(username)
+{
+    for (let i = 0; i < users.length; i++)
+    {
+        if (username === users[i]["username"])
+        {
+            currentUserIndex = i;
+            ShowAdminInterface(users[i].isAdmin);
+            SetFahrenheitCheckbox(users[i].useFahrenheit);
+            return;
+        }
+    }
+
+    alert("Der Benutzer wurde nicht gefunden!");
+}
+
+function FillData()
+{
+    const cityname = getParam(cityStorageName);
+    const weather = getWeatherForCity(cityname);
+
+    if (weather == undefined)
+    {
+        throw "Die Stadt wurde nicht gefunden";
+    }
+    else
+    {
+        SetCityName(cityname);
+    
+        SetTemperature(users[currentUserIndex].useFahrenheit ? weather.getFahrenheit() : weather.temp);
+        SetRain(weather.rainPossibility);
+    }
+}
+
+function ScriptOnDocLoad()
+{
+    SetPopupListeners();
+    LoadStorageData();
+    
+    const currentUser = getParam(userStorageName);
+    SetUser(currentUser);
+
+    // Bei diesem API-Call entsteht aufgrund der CORS-Regelung ein nicht behebbarer Fehler!
+    // Daher werden ersatzweise Standardwerte für das Wetter verwendet.
+    let weather = ConvertDWDResponseToObject(GetDWDWeatherData(cities[0]["apiKey"]))
+
+    console.log("PARAMS: " + location.search); // DEBUG
+    
+    try
+    {
+        FillData();
+    }
+    catch
+    {
+        // HTML Elemente nicht gefunden
+    }
+}
 window.onload = ScriptOnDocLoad;
